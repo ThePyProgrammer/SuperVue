b8f021a0f8c0cc4326d6b2952cad31016797a955
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 03:21:44 +0800

    Rename iss.ts as main.ts

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,14 +1,14 @@
-import { getPosition, getJoke } from "@/api/api";
+import { getPosition, getAstronauts } from "@/api/api";
 import {formatDate} from "@/util/datetimeutils";
 
 async function main(): Promise<string> {
   const pos = await getPosition();
   const {date, latitude, longitude} = pos;
   const dateAsString = formatDate(date);
-  return `On ${dateAsString}, The ISS was at a latitude of ${latitude} and longitude of ${longitude}`;
+  return `On ${dateAsString}, The ISS was at a latitude of ${latitude} and longitude of ${longitude}.`;
   // return "Hello, world!";
 }
 
 export default {
-  main, home: getJoke
+  main, getAstronauts
 };
