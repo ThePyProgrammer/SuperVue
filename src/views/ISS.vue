d5f38eb2c223ab8d3d54e9a2a93d6ac964184dc8
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 03:23:02 +0800

    Update ISS.vue

--- src/views/ISS.vue
+++ src/views/ISS.vue
@@ -1,45 +1,40 @@
 <template>
   <v-container fluid>
-<!--    <v-card>-->
-<!--      <div class="pa-8">-->
-<!--        <div align="center">-->
-<!--          <vue-code-highlight language="kotlin">-->
-<!--            <pre>fun hello(world: String) = "hello $world"</pre>-->
-<!--            <pre>fun hello&lt;T&gt;hello(world: () -> T) = "hello ${world()}"</pre>-->
-<!--          </vue-code-highlight>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </v-card>-->
     <v-card>
       <v-card-title>
-        <div v-if="name === ''">
-          Your profile
-        </div>
-        <div v-else>
-          Welcome to Vue, {{ name }}
+        <div>
+          International Space Station Data
         </div>
         <v-spacer/>
       </v-card-title>
       <div class="pa-8">
-        <div v-if="renderComponent">
-          Output of <code>iss</code>: {{ mainOut }}
-        </div>
-        <v-text-field
-          v-model="name"
-          label="Search a possible Joke"
-        >
-        </v-text-field>
         <div>
-          Output of <code>home</code>: {{ homeOut }}
+          {{ mainOut }}
         </div>
       </div>
     </v-card>
+
+    <v-list
+      dense
+      nav>
+      <v-list-item v-for="astronaut in astronauts" v-bind:key="astronaut">
+        <v-card>
+          <v-card-title>
+            <div>
+              <code>{{ astronaut }}</code>
+            </div>
+            <v-spacer/>
+          </v-card-title>
+        </v-card>
+        <v-spacer />
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import api from "@/api/iss";
+import api from "@/api/main";
 
 export default Vue.extend({
   name: "ISS-Home",
@@ -47,20 +42,12 @@ export default Vue.extend({
     return {
       name: "",
       mainOut: "",
-      homeOut: "",
-      renderComponent: true
+      astronauts: []
     };
-  },
-  methods: {
-    methodThatForcesUpdate() {
-      // ...
-      this.$forceUpdate();  // Notice we have to use a $ here
-      // ...
-    }
   },
   async mounted() {
     this.mainOut = await api.main();
-    this.homeOut = await api.home();
+    this.astronauts = await api.getAstronauts();
   }
 });
 
