0992fc0f3a72c9ad85e6055ac770afd285dc4b2c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 23:57:55 +0800

    Add Random Quotable Quote API Reference to Main.vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,4 +1,5 @@
 <template>
+  <v-flex>
   <v-container fluid>
     <v-card>
       <v-card-title>
@@ -9,14 +10,38 @@
         <v-spacer/>
       </v-card-title>
     </v-card>
+
+    <v-card>
+      <v-card-title class="text-wrap pa-8" style="white-space: normal; padding-bottom: 1rem;">
+          {{ content }}
+        <v-spacer/>
+      </v-card-title>
+      <div class="text-right pa-8">
+        ~ {{ author }}
+      </div>
+    </v-card>
   </v-container>
+  </v-flex>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import {getRandomQuotableQuote} from "@/api/api";
 
 export default Vue.extend({
-  name: "Home"
+  name: "Home",
+  data: function () {
+    return {
+      content: "",
+      author: ""
+    };
+  },
+  async mounted() {
+    const quote = await getRandomQuotableQuote();
+    this.content = quote.content;
+    this.author = quote.author;
+    console.log(this.content, this.author);
+  }
 });
 
 </script>
