ba6f822f148367bcae5981eb9743082c24db1416
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 16:21:33 +0800

    Create Main.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        <div>
+          <highlightjs language='kotlin' code="fun hello&lt;T&gt;(world: () -> T) = 'hello ${world}'" />
+          <pre><code class="kotlin"></code></pre>
+        </div>
+        <v-spacer/>
+      </v-card-title>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Home"
+});
+
+</script>
