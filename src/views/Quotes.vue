3228c00112cd3ac2177ddc0dd15c7ca1dc1c3ca5
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 16:21:45 +0800

    Create Quotes.vue

---
+++ src/views/Quotes.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        <div>
+          Quotable Quotes
+        </div>
+        <v-spacer/>
+      </v-card-title>
+    </v-card>
+
+    <v-list
+      dense
+      nav>
+      <v-list-item v-for="quote in quotes" v-bind:key="quote">
+        <v-card>
+          <v-card-title>
+            <div>
+              {{ quote.content }}
+            </div>
+            <div style="float:right;">
+              ~ {{ quote.author }}
+            </div>
+            <v-spacer/>
+          </v-card-title>
+        </v-card>
+        <v-spacer />
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { getAllQuotableQuotes } from "@/api/api";
+
+
+export default Vue.extend({
+  name: "QuotableQuotes",
+  data: function () {
+    return {
+      articles: []
+    };
+  },
+  async mounted() {
+    this.quotes = await getAllQuotableQuotes();
+  }
+});
+
+</script>
