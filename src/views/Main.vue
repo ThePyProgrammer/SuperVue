4de060fbf71f8e510257b98533c97b0f2fcf9c93
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-18 02:18:58 +0800

    Make the word break work normally

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,24 +1,24 @@
 <template>
   <v-flex>
   <v-container fluid>
-    <v-card>
-      <v-card-title>
-        <div>
-          <highlightjs language='kotlin' code="fun hello&lt;T&gt;(world: () -> T) = 'hello ${world}'" />
-          <pre><code class="kotlin"></code></pre>
-        </div>
-        <v-spacer/>
-      </v-card-title>
-    </v-card>
+<!--    <v-card>-->
+<!--      <v-card-title>-->
+<!--        <div>-->
+<!--          <highlightjs language='kotlin' code="fun hello&lt;T&gt;(world: () -> T) = 'hello ${world}'" />-->
+<!--          <pre><code class="kotlin"></code></pre>-->
+<!--        </div>-->
+<!--        <v-spacer/>-->
+<!--      </v-card-title>-->
+<!--    </v-card>-->
 
     <v-card>
-      <v-card-title class="text-wrap pa-8" style="white-space: normal; padding-bottom: 1rem;">
-          {{ content }}
-        <v-spacer/>
+      <v-card-title class="pa-8" style="word-break: normal">
+        {{ content }}
       </v-card-title>
       <div class="text-right pa-8">
         ~ {{ author }}
       </div>
+      <v-spacer />
     </v-card>
   </v-container>
   </v-flex>
