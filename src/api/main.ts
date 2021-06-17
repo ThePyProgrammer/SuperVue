dd48d22ec18e1fa27bd3e1122ef76371673ff0aa
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-06-17 16:17:34 +0800

    Rename datetimeutils to just datetime

--- src/api/main.ts
+++ src/api/main.ts
@@ -1,12 +1,11 @@
 import { getPosition, getAstronauts } from "@/api/api";
-import {formatDate} from "@/util/datetimeutils";
+import {formatDate} from "@/util/datetime";
 
 async function main(): Promise<string> {
   const pos = await getPosition();
   const {date, latitude, longitude} = pos;
   const dateAsString = formatDate(date);
   return `On ${dateAsString}, The ISS was at a latitude of ${latitude} and longitude of ${longitude}.`;
-  // return "Hello, world!";
 }
 
 export default {
